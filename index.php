c50f8c9faa7da88c0e685979d9de3eb4173c3175
mattbag <wised@MacBook-Air-di-bagni.local>
2016-11-05 12:36:56 +1100

    removed scripts

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-		<script src="//use.typekit.net/yjd8lkq.js"></script><script>try{Typekit.load();}catch(e){}</script>
-		<script src="https://maps.googleapis.com/maps/api/js"></script>
+		<!-- <script src="//use.typekit.net/yjd8lkq.js"></script><script>try{Typekit.load();}catch(e){}</script> -->
+		<!-- <script src="https://maps.googleapis.com/maps/api/js"></script> -->
 		<?php if ( defined( 'HM_DEV' ) ) : ?>
 			<script src="//localhost:35729/livereload.js"></script>
 		<?php endif; ?>
